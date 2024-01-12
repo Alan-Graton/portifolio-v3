5684e2359d57f37041b9169a9e478b4d672fe137
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2024-01-12 14:43:53 -0300

    feat: config github actions for gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import wyw from "@wyw-in-js/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://Alan-Graton.github.io/portifolio-v3',
   plugins: [react(), wyw()],
 });
