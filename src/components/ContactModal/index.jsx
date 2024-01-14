a0081589efe0a02351b4c057fcc07740b5404e93
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2024-01-14 00:11:08 -0300

    feat-wip: creating a contact modal

---
+++ src/components/ContactModal/index.jsx
@@ -0,0 +1,46 @@
+import "./styles.css";
+
+export function ContactModal({ visible, setVisible }) {
+  const overlayClassName = visible
+    ? "modal-overlay-visible"
+    : "modal-overlay-hidden";
+
+  return (
+    <>
+      <div className={overlayClassName}>
+        <div className="contact-modal">
+          <div className="contact-modal content">
+            <div className="contact-modal header">
+              <p>Que tal me mandar em e-mail?</p>
+              <button className="close-btn" onClick={() => setVisible(false)}>
+                X
+              </button>
+            </div>
+            <div className="contact-modal body">
+              <input
+                className="guestbook-name"
+                placeholder="Seu Nome"
+                type="text"
+              />
+              <textarea
+                className="guestbook-message"
+                placeholder="Sua mensagem..."
+                cols="30"
+                rows="5"
+                required
+              />
+            </div>
+            <div className="contact-modal footer">
+              <button className="cancel-btn" onClick={() => setVisible(false)}>
+                Deixa pra lá
+              </button>
+              <button className="submit-btn" onClick={() => setVisible(false)}>
+                Enviar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
